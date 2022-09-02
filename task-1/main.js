dd33cdfbe7a9cfb7421a5db8fda2e1272254b7b2
agacan123 <110633844+agacan123@users.noreply.github.com>
2022-09-02 11:43:17 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,25 +1,24 @@
 // Use this arrays for solution
 let array_1 = [3, 1, 3, 4, 2, 4, 12]
 let array_2 = [4, 15, 43, 1, 15, 1]
-
 let arr = [];
 
 for (let i = 0; i < array_1.length; i++){
 
- const element =  array_1[i]
+ const element =  array_1[i];
 let exist = false
-}
+
 for (let j = 0; j < array_2.length; j++){
-const next_element = array_2[j]
+const next_element = array_2[j];
 if (element == next_element){
- exist = true
-    break
+ exist = true;
+    break;
 }
 
 
 }
 if (!exist){
     arr.push(element);
-
+}
 }
 console.log(arr);
