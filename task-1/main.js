8a0710fec41a6cc1d0a2132aaaabb7e19641a159
agacan123 <110633844+agacan123@users.noreply.github.com>
2022-09-02 11:21:47 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -2,4 +2,25 @@
 let array_1 = [3, 1, 3, 4, 2, 4, 12]
 let array_2 = [4, 15, 43, 1, 15, 1]
 
-// Write code here. 
+
+let arr = [];
+
+for (let i = 0; i < array_1.length; i++){
+
+ const element = parseInt( array_1[i])
+let exist = false
+}
+for (let j = 0; j < array_2.length; j++){
+const next_element = parseInt(array_2[j])
+if (element == next_element){
+ exist = true
+    break
+}
+
+
+}
+if (!exist){
+    array_1.push(element);
+
+}
+console.log(arr);
