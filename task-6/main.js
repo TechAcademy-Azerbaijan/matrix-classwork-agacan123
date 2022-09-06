4de0f59048dc546c032761918630e0f9dcffb9ba
agacan123 <110633844+agacan123@users.noreply.github.com>
2022-09-06 10:38:28 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,15 +1,26 @@
 // Use this matrices for solution
+const { setMaxListeners } = require("prompt");
 
-let matrix_1 = [
+let prices = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]; 
 
-let matrix_2 = [
+let seats = [
     [1, 0, 1],
     [0, 1, 0],
     [1, 0, 1]
 ]; 
+let sum = 0
+ for (let i = 0; i < seats.length; i++){
+let row = seats [i]
+for (let j = 0; j < row.length; j++){
+     const seat = seats[i][j]
+if (seat ===1){
+    sum += prices[i][j]
+}
+}
 
-// Write code here. 
+ }
+ console.log(sum);
